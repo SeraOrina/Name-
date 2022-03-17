accf78d5528c9f0cae8a2f2e31ab0dda08e2fa73
Sera <seramokeira@gmail.com>
2022-03-17 13:35:32 +0300

    feature;add js to prevent default

--- js/styles.js
+++ js/styles.js
@@ -1,1 +1,7 @@
-document.getElementById
+const name=document.getElementById("date")
+const password=document.getElementById("month")
+const form=document.getElementById("form")
+
+form.addEventListener("submit",(e)=> {
+    e.preventDefault()
+})
