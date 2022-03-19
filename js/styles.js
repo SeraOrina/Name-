563b9bc6030ca8c763ea53aecc3f34b04672d2e8
Sera <seramokeira@gmail.com>
2022-03-19 07:06:05 +0300

    feature;add alert messages

--- js/styles.js
+++ js/styles.js
@@ -1,11 +1,31 @@
 
-  const date= document.getElementById("date")
+  let  date= document.getElementById("date")
   const month=document.getElementById("month")
   const year=document.getElementById("year")
   const form=document.getElementById("form")
 
   form.addEventListener("submit",(e) => {
     e.preventDefault()
-          
+       
+    if(date.value === "") {
+        alert("DATE IS REQUIRED")
+    }
+    if (date.value <0 || date.value>31) {
+        alert("INVALID DATE")
+    }
+    
+   
      
   })
+  
+      
+
+
+
+
+
+
+
+
+
+
